6e0fcac70e72b25ae9b953c5543f7448c5c90020
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2023-12-02 06:59:26 +0500

    Refactor navbar and add sections to home page

--- src/app/navbar/page.tsx
+++ src/app/navbar/page.tsx
@@ -1,26 +1,85 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import Logo from './logo.svg';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Navbar() {
+    const [ navbar, setNavbar] = useState(false);
     return (
-        <nav className="navbar flex flex-wrap p-3 bg-slate-300 m-5 rounded-full justify-between">
-            <div className="navbar__logo align-middle">
-                <img src={logo} alt="logo" />
-            </div>
-            <div className="navbar__menu">
-                <ul className="flex gap-x-10  menu-item text-2xl font-bold">
-                    <button className="bg-slate-400 p-3 rounded-full">Products</button>
-                    <button className="bg-slate-400 p-3 rounded-full">Home</button>
-                    <button className="bg-slate-400 p-3 rounded-full">Services</button>
-                    <button className="bg-slate-400 p-3 rounded-full">Contact</button>
-                </ul>
-            </div>
-            <div className="navbar__search flex">
-                <input
-                    type="text"
-                    placeholder="Search Bar"
-                    className="bg-slate-400 p-3 rounded-full"
-                />
-            </div>
+        // <nav className="navbar flex flex-wrap p-3 bg-slate-300 m-5 rounded-full justify-between">
+        //     <div className="navbar__logo align-middle">
+        //         <img src={logo} alt="logo" />
+        //     </div>
+        //     <div className="navbar__menu">
+        //         <ul className="flex gap-x-10  menu-item text-2xl font-bold">
+        //             <button className="bg-slate-400 p-3 rounded-full">Products</button>
+        //             <button className="bg-slate-400 p-3 rounded-full">Home</button>
+        //             <button className="bg-slate-400 p-3 rounded-full">Services</button>
+        //             <button className="bg-slate-400 p-3 rounded-full">Contact</button>
+        //         </ul>
+        //     </div>
+        //     <div className="navbar__search flex">
+        //         <input
+        //             type="text"
+        //             placeholder="Search Bar"
+        //             className="bg-slate-400 p-3 rounded-full"
+        //         />
+        //     </div>
+        // </nav>
+        <div>
+            <nav className='w-full bg-black fixed top-0 left-0 right-0 z-10'>
+                <div className='justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8'>
+                    <div>
+                        <div className='flex items-center justify-between py-3 md:py-5 md:block'>
+                            {/*logo*/}
+                            <Link href='/'>
+                                <h2 className='text-2xl text-cyan-600 font-bold'>Logo</h2>
+                            </Link>
+                            {/* hamburger menu */}
+                            <div className='md:hidden'>
+                                <button type='button' className='p-2 text-gray-700 rounded-md outline-none focus:border-gray-400'>
+                                    <button>
+                                        {navbar ? (
+                                            <Image src="./close.svg" alt='logo' width={30} height={30} />
+                                        ) : (
+                                            <Image
+                                                src='/menu.svg'
+                                                alt='logo'
+                                                width={30}
+                                                height={30}
+                                                className='focus:border-none active:border-none'
+                                            />
+                                        )}
+                                    </button>
+                                    <>
+                                        {navbar ? (
+                                            <Image src="./close.svg" alt='logo' width={30} height={30} />
+                                        ) : (
+                                            <Image
+                                                src='/menu.svg'
+                                                alt='logo'
+                                                width={30}
+                                                height={30}
+                                                className='focus:border-none active:border-none'
+                                            />
+                                        )}
+                                    </>
+                                </button>
+                                {navbar ? (
+                                    <Image src="./close.svg" alt='logo' width={30} height={30} />
+                                ) : (
+                                    <Image
+                                        src='/menu.svg'
+                                        alt='logo'
+                                        width={30}
+                                        height={30}
+                                        className='focus:border-none active:border-none'
+                                    />
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
         </nav>
-    );
-}
+        </div>
+    )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,28 @@ import Navbar from "./navbar/page";
 
 export default function Home() {
   return (
-     <Navbar/>
+    //  <Navbar/>
+    <div>
+      <section
+        id="about"
+        className="text-white h-screen bg-black text-center flex">
+        <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold">Hero</h1>
+      </section>
+      <section
+        id="blog"
+        className="text-white h-screen bg-black text-center flex">
+        <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold">Blog</h1>
+      </section>
+      <section
+        id="contact"
+        className="text-white h-screen bg-black text-center flex">
+        <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold">Contact</h1>
+      </section>
+      <section
+        id="projects"
+        className="text-white h-screen bg-black text-center flex">
+        <h1 className="m-auto font-waterfall text-7xl text-purple-600 font-bold">Projects</h1>
+      </section>
+    </div>
   )
 }
