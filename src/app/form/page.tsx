6106f421a6903a2ded0d0732d99739139c2f6234
AbbasAliKazmi <syedabbasali1998.aa@gmail.com>
2024-02-10 07:45:07 +0500

    Update page title in form component

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -7,7 +7,7 @@ import { Ribbon } from 'lucide-react';
       <div className="bg-slate-600 h-screen w-screen flex flex-col">
         <div className="bg-black w-4/5 h-96 px-20 pb-20 pt-16 mx-auto">
           <Ribbon className='mx-auto w-20 h-20' />
-          <h1 className="text-5xl font-bold text-white">Let’s talk about your</h1>
+          <h1 className="text-5xl font-bold text-white">Let’s arrange your</h1>
           <h1 className="text-5xl font-bold text-white mt-5">Dream Wedding</h1>
         </div>
       </div>
